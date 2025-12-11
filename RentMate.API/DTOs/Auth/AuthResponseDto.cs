5c0fae793978e649c5c1bddab03d1d806ba9f63a
Adham Hussien <113741853+adham-hussien33@users.noreply.github.com>
2025-12-11 11:36:06 +0200

    Add files via upload

---
+++ RentMate.API/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,11 @@
+namespace RentMate.API.DTOs.Auth
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+} 
